0824fa01d0398caccec985b2e6f21d1a7e44ae31
juwel119 <juwelcse18@gmail.com>
2023-08-28 20:17:22 +0600

    first commit

---
+++ application/views/footer.php
@@ -0,0 +1,5 @@
+    <div style="clear:both;color:#aaa; padding:20px;">
+
+    	<hr /><center>&copy; <?php echo date ('Y'); ?> Medical Information System- Developed by Md Juwel Rana</center>
+
+    </div>
